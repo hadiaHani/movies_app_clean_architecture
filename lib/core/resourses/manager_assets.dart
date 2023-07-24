b0a128f9929050ee6efd5198b45c8165e8fbf448
hadiaHani <hh7113404@gmail.com>
2023-07-24 15:59:55 +0300

    ManagerJson

---
+++ lib/core/resourses/manager_assets.dart
@@ -0,0 +1,9 @@
+const String imagesPath = "assets/images";
+const String jsonPath = 'assets/json';
+const String translationPath = 'assets/translation';
+
+class ManagerJson {
+  static const String loading = '$jsonPath/loading.json';
+  static const String error = '$jsonPath/error.json';
+  static const String success = '$jsonPath/success.json';
+}
