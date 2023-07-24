164f2ce77342569b380f45ea4598d2dd2f4291d1
hadiaHani <hh7113404@gmail.com>
2023-07-24 15:55:40 +0300

    dummy movies data

---
+++ lib/features/movies_detailes/presentation/view/dummy.dart
@@ -0,0 +1,45 @@
+import 'package:movies_app/features/movies_detailes/domain/model/movie_detailes_model.dart';
+import 'package:movies_app/features/movies_detailes/domain/model/recommendation.dart';
+
+MovieDetailesModel movieDetailDummy = MovieDetailesModel(
+  backdropPath: "/nmGWzTLMXy9x7mKd8NKPLmHtWGa.jpg",
+  genres: [
+    GenresModel(name: 'Family', id: 10751),
+  ],
+  id: 438148,
+  overview:
+      "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
+  releaseDate: "2022-06-29",
+  runtime: 87,
+  title: "Minions: The Rise of Gru",
+  voteAverage: 7.8,
+);
+
+List<RecommendationModel> recommendationDummy = [
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+  RecommendationModel(
+      backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg", id: 924482),
+];
